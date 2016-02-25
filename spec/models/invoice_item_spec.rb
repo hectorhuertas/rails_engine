1c48c5551c4954936bf19d156cc72babbabb9f88
Hector Huertas <hectorhuertas@gmail.com>
2016-02-25 10:11:14 -0700

    Fix pending tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe InvoiceItem, :type => :model do
   it "saves the unit_price as dollars instead of cents" do
-    pending
     invoice_item = InvoiceItem.create(unit_price: 12345)
-    expect(invoice_item.reload.unit_price).to eq('123,45')
+    expect(invoice_item.unit_price.to_s).to eq('123.45')
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
   it "saves the unit_price as dollars instead of cents" do
-    pending
     item = Item.create(unit_price: 12345)
-    expect(item.reload.unit_price).to eq('123,45')
+    expect(item.reload.unit_price.to_s).to eq('123.45')
   end
 
   it "has an invoice_item" do
